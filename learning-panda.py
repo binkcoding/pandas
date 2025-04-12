77f0b3279f91fc0a0a1e1acd48adac1c5e7c5977
binkcoding <malachysutton@gmail.com>
2025-04-12 18:46:43 +1000

    General functions

--- learning-panda.py
+++ learning-panda.py
@@ -20,8 +20,21 @@ print(df.duplicated().sum())
 #Removing duplicats
 df_no_duplicats = df.drop_duplicates()
 
+#Convert data types to other data types
+# df['Column 1'] = df['Column1'].astype(float)
+
+#Convert categorical data to numerical data
+# df_encode = pd.get_dummies(df, columns=[car_brand])
+
+#Identifying outliers
+# Q1 = df["precipitation"].quantile(0.25)
+# Q3 = df["temp_max"].quantile(0.75)
+# IQR = Q3 - Q1
 
 
-print(df.head())
-print(df.describe())
-print(df.info())
+#Find out what each column data type is
+# print(df.dtypes)
+
+# print(df.head())
+# print(df.describe())
+# print(df.info())
