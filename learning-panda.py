2ab290402ff8e3003548fa42610eaf8dccb51b5f
binkcoding <malachysutton@gmail.com>
2025-04-12 18:25:12 +1000

    Initial commit Cleaning data and ensuring only trying to get the mean of numerical data only as to not get errors

---
+++ learning-panda.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("seattle-weather.csv")
+
+#Check for any missing values
+print(df.isnull().sum())
+
+#Drop rows with missing values and assign to value "df cleaned"
+df_cleaned = df.dropna()
+
+#Fill missing values with mean for numerical data and assign to variable df filled
+df_filled = df.fillna(df.mean(numeric_only=True))
+
+print(df.head())
+print(df.describe())
+print(df.info())
